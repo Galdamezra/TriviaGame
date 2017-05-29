58c99a76ce878f4afc9e4a08cacf456d15d495ec
Rafael Galdamez <rafaelgaldamez@Rafaels-MacBook-Pro.local>
2017-05-28 21:23:38 -0700

    final updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,14 +4,15 @@
 //Score variables
 var correctScore    = 0;
 var incorrectScore  = 0;
-var unansweredScore = 0;
 
 //Functions
 //========================================
 
 //Timer start on click
+window.onload = function() {
+  $("#startbutton").click(stopwatch.start);
 
-$("#startbutton").click(start);
+};
 
 //variable will hold our setInterval that runs the watch
 var intervalId;
@@ -30,6 +31,18 @@ var stopwatch = {
       counter = setInterval(stopwatch.count, 1000);
 
     }
+
+  },
+  stop: function() {
+
+    //  Stop the counter at 00:00 and alert game over
+    if(--time < 0) {
+
+      alert("GAME OVER!");
+      clearInterval(counter);
+
+    }
+
   },
   count: function() {
 
@@ -70,21 +83,158 @@ var stopwatch = {
 
 
 
+//Check if user won or lost
+//Check if user won or lost and reset the game
+var checkWin = function() {
+
+  //Check if currentScore is larger than targetScore
+  if(incorrectScore > correctScore) {
+    alert("Sorry. You Lost!");
+    console.log("you lost");
+
+    //Restart game
+    startGame();
+
+  }
+
+  else if(correctScore > incorrectScore) {
+    alert("Congratulation! You Won!");
+    console.log("You Won!");
+
+    //Restart game
+    startGame();
+
+  }
+
+}
+
+
+//Start Game function
 var startGame = function(){
 
   //Reset Current Score
   var correctScore    = 0;
   var incorrectScore  = 0;
-  var unansweredScore = 0;
+
+  //if statement to define scores for each Questions
+
+  //change hTML to reflect score changes
+  $("#score").html('<h3> Correct Score: ' + correctScore + '</h3><br><h3>Incorrect Score: ' + incorrectScore + '</h3>');
+
 
   //Testing
 
 };
 
+//Respond to clicks
+var addValues = function (correctScore) {
+
+  // Change the HTML to reflect changes in currentScore
+  $("#score").html(correctScore);
+
+  //call the checkWin function
+  checkWin();
+
+  //Testing console
+  console.log("Correct Score: " + correctScore);
+
+}
+
+//Respond to clicks
+var addValues = function (incorrectScore) {
+
+  // Change the HTML to reflect changes in currentScore
+  $("#score").html(incorrectScore);
+
+  //call the checkWin function
+  checkWin();
+
+  //Testing console
+  console.log("Incorrect Score: " + incorrectScore);
+
+}
+
 //MAIN PROCESS
 //========================================
 
-//click function for Start button
-$("#startbutton").click(function(){
-  alert("test")
+startGame();
+
+$("#question1false").click(function() {
+  correctScore++
+  console.log(correctScore)
+});
+
+$("#question1true").click(function() {
+  incorrectScore++
+  console.log(correctScore)
+});
+
+$("#question2true").click(function() {
+  correctScore++
+  console.log(correctScore)
+});
+
+$("#question2false").click(function() {
+  incorrectScore++
+  console.log(correctScore)
+});
+
+$("#question3false").click(function() {
+  correctScore++
+  console.log(correctScore)
+});
+
+$("#question3true").click(function() {
+  incorrectScore++
+  console.log(correctScore)
+});
+
+$("#question4false").click(function() {
+  correctScore++
+  console.log(correctScore)
+});
+
+$("#question4true").click(function() {
+  incorrectScore++
+  console.log(correctScore)
+});
+
+$("#question5true").click(function() {
+  correctScore++
+  console.log(correctScore)
+});
+
+$("#question5false").click(function() {
+  incorrectScore++
+  console.log(correctScore)
+});
+
+$("#question6true").click(function() {
+  correctScore++
+  console.log(correctScore)
+});
+
+$("#question6false").click(function() {
+  incorrectScore++
+  console.log(correctScore)
+});
+
+$("#question7true").click(function() {
+  correctScore++
+  console.log(correctScore)
+});
+
+$("#question7false").click(function() {
+  incorrectScore++
+  console.log(correctScore)
+});
+
+$("#question8true").click(function() {
+  correctScore++
+  console.log(correctScore)
+});
+
+$("#question8false").click(function() {
+  incorrectScore++
+  console.log(correctScore)
 });
